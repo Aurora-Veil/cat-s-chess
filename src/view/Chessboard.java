ffc2195aa7998ec4076409a34d576ddea358471b
Olivia <106416908+Oliviawww@users.noreply.github.com>
2022-05-30 15:22:27 +0800

    Ver 2.2.2 添加了音效

--- src/view/Chessboard.java
+++ src/view/Chessboard.java
@@ -6,8 +6,13 @@ import model.*;
 import controller.ClickController;
 
 import javax.swing.*;
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
+import java.io.File;
 import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -144,6 +149,8 @@ public class Chessboard extends JComponent {
         recorder.add(step);
         chessComponents[row1][col1].repaint();
         chessComponents[row2][col2].repaint();
+        MusicPlay bing = new MusicPlay("./src/轻轻按下开关_1.wav");
+        bing.musicMain(1);
     }
 
     public void initiateEmptyChessboard() {
@@ -379,5 +386,47 @@ public class Chessboard extends JComponent {
         }
         recorder.remove(recorder.size() - 1);
         swapColor();
+    }
+    public static class MusicPlay {
+
+        private AudioClip aau;
+        private String url;
+
+
+        public MusicPlay(String url){
+            this.url=url;
+            playMusic();
+
+        }
+        public  void playMusic(){
+            try {
+                URL cb;
+                //可以在项目里创建一个Source folder包，将音乐文件放到这个包里,再把路径给它
+                File f = new File(url); //url这里放音乐路径。
+                cb = f.toURL();
+                aau = Applet.newAudioClip(cb);
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            }
+        }
+        //控制音乐的方法,调用这个方法要给一个int参数
+        public void musicMain(int n) {
+            //用switch循环
+            switch (n) {
+                //如果是1就开始播放
+                case 1 -> aau.play();
+
+                //如果是2,就停止播放
+                case 2 -> aau.stop();
+                case 3 ->
+                    //循环播放
+                        aau.loop();
+                default -> {
+                }
+            }
+        }
+
     }
 }
